2250a177f9bc6fd1baadb223631825dae6cc86ce
Fendo181 <fendo181git@gmail.com>
2018-05-01 07:47:27 +0900

    LinkFactoryにテストデータを追加するように処理を記述した。

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -5,5 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Link::class, function (Faker $faker) {
     return [
         //
+        'title' => substr($faker->sentence(2),0,-1),
+        'url'   => $faker->url,
+        'description' => $faker->paragraph,
     ];
 });
