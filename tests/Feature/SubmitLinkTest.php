07ac22825ad4e72e0b4e91c87d56eb606bfcd934
Fendo181 <fendo181git@gmail.com>
2018-05-02 07:58:59 +0900

    guest_can_submit_a_new_linkのテストを追加

--- tests/Feature/SubmitLinkTest.php
+++ tests/Feature/SubmitLinkTest.php
@@ -18,9 +18,28 @@ class SubmitLinkTest extends TestCase
 //        $this->assertTrue(true);
 //    }
 
+   use RefreshDatabase;
+
     /** @test */
     function guest_can_submit_a_new_link()
     {
+        $response = $this->post('/submit',[
+           'title' => 'Example Title',
+            'url'  => 'http://example.com',
+            'description' => 'Example description',
+        ]);
+
+        $this->assertDatabaseHas('links',[
+           'title' => 'Example Title'
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertHeader('Location',url('/'));
+
+        $this
+            ->get('/')
+            ->assertSee('Example Title');
     }
 
     /** @test */
