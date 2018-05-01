b7bfd4137626022e59d880483f211fc91925ab7a
Fendo181 <fendo181git@gmail.com>
2018-05-02 07:48:35 +0900

    add Feature Test SubmitLinkTest
    テストの雛形を作成

--- tests/Feature/SubmitLinkTest.php
+++ tests/Feature/SubmitLinkTest.php
@@ -13,8 +13,34 @@ class SubmitLinkTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+//    public function testExample()
+//    {
+//        $this->assertTrue(true);
+//    }
+
+    /** @test */
+    function guest_can_submit_a_new_link()
     {
-        $this->assertTrue(true);
+    }
+
+    /** @test */
+    function link_is_not_created_if_validation_fails()
+    {
+    }
+
+    /** @test */
+    function link_is_not_created_with_an_invalid_url()
+    {
+    }
+
+    /** @test */
+    function  max_length_fails_when_too_long()
+    {
+
+    }
+
+    /** @test */
+    function max_length_succeeds_when_under_max()
+    {
     }
 }
