c87cc8250d882c03e06341d7ae1504d774f1d309
endu <futoshi.endo@pepabo.com>
2018-12-05 13:41:44 +0900

    Bootstrap4を導入した

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div>
             <h1>Submit a Link </h1>
-            <form action="/submit" method="post">
+            <form action="/submit" method="post" class="needs-validation" novalidate>
                 @if ($errors->any())
                     <div class="alert alert-danger" role="alert">
                         Plese fix the following errors
@@ -18,7 +18,9 @@
                     <label for="title">Title</label>
                     <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ old('title') }}">
                     @if($errors->has('title'))
-                        <span class="help-block">{{ $errors->first('title') }}</span>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('title') }}
+                        </div>
                     @endif
                 </div>
 
@@ -26,7 +28,11 @@
                     <label for="url">Url</label>
                     <input type="text" class="form-control" id="url" name="url" placeholder="URL" value="{{ old('url') }}">
                     @if($errors->has('url'))
-                        <span class="help-block">{{ $errors->first('url') }}</span>
+                        <span>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('url') }}
+                            </div>
+                        </span>
                     @endif
                 </div>
 
@@ -34,11 +40,13 @@
                     <label for="description">Description</label>
                     <textarea class="form-control" id="description" name="description" placeholder="description">{{ old('description') }}</textarea>
                     @if($errors->has('description'))
-                        <span class="help-block">{{ $errors->first('description') }}</span>
+                        <div class="invalid-feedback">
+                        {{ $errors->first('description') }}
+                        </div>
                     @endif
                 </div>
 
-                <button type="submit" class="btn btn-default">Submit</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
